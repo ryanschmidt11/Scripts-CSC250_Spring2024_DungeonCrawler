4044055d2319a865345021b851dc14f73d42c2b2
ryanschmidt11 <ryan.schmidt@cuw.edu>
2024-02-16 10:42:55 -0600

    changes

--- MySingleton.cs
+++ MySingleton.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class MySingleton
 {
     public static int secretNumber = -1;
+    internal static object currentDirection;
 }
--- Player.cs
+++ Player.cs
@@ -3,31 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
 
-public class Player : MonoBehaviour
+public class player : MonoBehaviour
 {
-    public GameObject Exit1;
-    private float speed = 0.1f;
-    public GameObject Player;
+    public GameObject northExit;
+    public GameObject southExit;
+    public GameObject eastExit;
+    public GameObject westExit;
+    private float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(!MySingleton.currentDirection.Equals("?"))
+        {
+            if (!MySingleton.currentDirection.Equals("north"))
+            {
+                this.gameObject.transform.position = this.southExit.transform.position = this.southExit.transform.position;
+            }
+        }
     }
 
 
-    private void OnTriggerEnter(Collider Exit1)
+    private void OnTriggerEnter(Collider other)
     {
-        //print("Secret Number = " + MySingleton.secretNumber);
-        //MySingleton.secretNumber = 5;
-        EditorSceneManager.LoadScene("Room1");
-        EditorSceneManager.MoveGameObjectsToScene(Player, Room1);
+        if(other.CompareTag("door"))
+        {
+            MySingleton.currentDirection = "north";
+            EditorSceneManager.LoadScene("DungeonRoom");
+        }
     }
 
     
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.Exit1.transform.position, this.speed);
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.northExit.transform.position, this.speed * Time.deltaTime);
     }
 }
